a99a8ec5f558f852c3524070aad2186df7f41e45
tsubo00 <tsubo@pixiv.com>
2017-03-25 16:21:10 +0900

    Deviseによる認証を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :comments
   get 'pages/info'
 
-  root to: redirect('/ideas')
+  root to: redirect('pages/info')
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
